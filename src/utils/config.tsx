9ae32fb655b34b065be6f1247e161ea987acb814
Nesz-ux <nhernandez1902@gmail.com>
2025-04-07 10:37:40 -0600

    Apuntador hacia el back web

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "http://192.168.1.168:5000";
+export const API_BASE_URL_Local = "http://192.168.1.168:5000";
+
+export const API_BASE_URL = "https://qrback.domcloud.dev";
